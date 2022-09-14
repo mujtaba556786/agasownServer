9a5c65fa182e01d98304b9ed4a415ecd9f96f1ca
Dhirender <dhirenderrawat@forcebolt.com>
2022-09-14 20:01:06 +0530

    Added Functionality for reset Password in accounts.py Added Serializers for reset forget in auth_serializer.py Added urls for reset password in odata/urls.py

--- odata/urls.py
+++ odata/urls.py
@@ -1,3 +1,5 @@
+from django.conf.urls import url
+
 from .sitemaps import *
 from django.conf.urls.static import static
 from django.conf import settings
@@ -6,7 +8,7 @@ from django.urls import path, include
 
 from rest_framework import routers
 from rest_framework.routers import SimpleRouter, Route
-from odata.views.apis import (
+from .views.apis import (
     ProductViewSet,
     CustomerViewSet,
     CategoryViewSet,
@@ -17,10 +19,9 @@ from odata.views.accounts import (
     LoginViewSet,
     SignupViewSet,
     LogoutViewSet,
-    UserForgotPasswordViewSet,
-    VerifyUserForgotPasswordViewSet,
-    ResetPasswordViewSet,
-    ResetPasswordViewSet,
+    UserForgotPassword,
+    VerifyUserForgotPassword,
+    ResetPassword,
 )
 from odata.views.pg_stripe import (
     CreateCheckoutSession,
@@ -28,8 +29,10 @@ from odata.views.pg_stripe import (
     StripeCard,
     # StripeWebHookView,
     success,
-    
+
 )
+
+# from odata.views.reset_password import PasswordTokenCheck
 
 viewset_dict = {
     "get": "list",
@@ -77,7 +80,6 @@ product_list = ProductViewSet.as_view(viewset_dict)
 customer_list = CustomerViewSet.as_view(viewset_dict)
 category_list = CategoryViewSet.as_view(viewset_dict)
 
-
 router = routers.DefaultRouter()
 # router.register(r'products', ProductViewSet, r"tool")
 
@@ -86,7 +88,6 @@ sitemaps_dict = {
     "product": Product_Sitemap,
     # 'order' : Order_Sitemap,
 }
-
 
 router.register(r"products", ProductViewSet),
 router.register(r"payments", PaymentViewset),
@@ -98,27 +99,31 @@ router.register(r"category", CategoryViewSet),
 router.register(r"login", LoginViewSet, basename="login")
 router.register(r"logout", LogoutViewSet, basename="logout")
 router.register(r"sign-up", SignupViewSet, basename="sign-up")
-router.register(
-    r"forgot-password", UserForgotPasswordViewSet, basename="forgot-password"
-)
-router.register(
-    r"verify-forgot-password",
-    VerifyUserForgotPasswordViewSet,
-    basename="verify-forgot-password",
-)
-router.register(r"reset-password", ResetPasswordViewSet, basename="reset-password")
+# router.register(
+#     r"forgot-password", UserForgotPasswordViewSet, basename="request-reset-email"
+# )
+# router.register(
+#     r"verify-forgot-password/<uidb64>/token",
+#     VerifyUserForgotPasswordViewSet,
+#     basename="password-reset-confirm",
+# )
+# router.register(r"reset-password", ResetPasswordViewSet, basename="reset-password")
 urlpatterns = [
-    # path("api/", include(router.urls)),
-    path(
-        "sitemap.xml",
-        sitemap,
-        {"sitemaps": sitemaps_dict},
-        name="django.contrib.sitemaps.views.sitemap",
-    ),
-    path("", include(router.urls)),
-    path("stripe/", StripeCard.as_view()),
-    path("stripe/check-out", StipeCheckoutSession.as_view()),
-    path("stripe/create-checkout", CreateCheckoutSession.as_view()),
-    # path("stripe/webhook", StripeWebHookView.as_view()),
-    path("stripe/success", success),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                  url(r'^api/', include(router.urls)),
+                  path(
+                      "sitemap.xml",
+                      sitemap,
+                      {"sitemaps": sitemaps_dict},
+                      name="django.contrib.sitemaps.views.sitemap",
+                  ),
+                  path("", include(router.urls)),
+                  path("stripe/", StripeCard.as_view()),
+                  path("stripe/check-out", StipeCheckoutSession.as_view()),
+                  path("stripe/create-checkout", CreateCheckoutSession.as_view()),
+                  # path("stripe/webhook", StripeWebHookView.as_view()),
+                  path("stripe/success", success),
+                  path("request-reset-email/", UserForgotPassword.as_view(), name="request-reset-email"),
+                  path("password-reset/<uidb64>/<token>/", VerifyUserForgotPassword.as_view(),
+                       name='password-reset-confirm'),
+                  path('reset-password/', ResetPassword.as_view(), name='reset-password')
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
