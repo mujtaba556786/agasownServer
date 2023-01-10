73e3b7ecab3172186238bbb58c47dfd436792973
DhirenderForcebolt <dhirenderrawat@forcebolt.com>
2023-01-10 22:47:10 +0530

    added size, color, color_de in Product and and voucher_value and discount_value in customer table and getting customer by user_id

--- odata/models.py
+++ odata/models.py
@@ -10,19 +10,20 @@ from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 from django.shortcuts import reverse
 from djongo import models
-#18.216.224.202u
+
+# 18.216.224.202u
 
 Available_Size = (
     ('S', 'small'),
     ('M', 'medium'),
     ('L', 'large'),
-  ('XL', 'extra large'),
+    ('XL', 'extra large'),
 )
 Available_Color = (
-    ('w','white'),
-    ('b','black'),
-    ('g','green'),
-    ('y','yellow'),
+    ('w', 'white'),
+    ('b', 'black'),
+    ('g', 'green'),
+    ('y', 'yellow'),
 )
 
 Trasaction_status = (
@@ -40,12 +41,14 @@ RANKING = (
     ('promoted', 'Promoted'),
     ('best_seller', 'Best Seller'),
 )
+
+
 class Categories(models.Model):
     objects = models.DjongoManager()
     _id = models.ObjectIdField(primary_key=True)
     parent = models.ForeignKey("self", on_delete=models.CASCADE, null=True, blank=True)
-    category_name = models.CharField(max_length=100,null=True,blank=True)
-    category_name_de = models.CharField(max_length=100,null=True,blank=True)
+    category_name = models.CharField(max_length=100, null=True, blank=True)
+    category_name_de = models.CharField(max_length=100, null=True, blank=True)
     # picture = models.ImageField(null=True,blank=True, upload_to="images")
     picture = models.CharField(max_length=1000, null=True, blank=True)
     active = models.BooleanField(default=False)
@@ -58,38 +61,41 @@ class Categories(models.Model):
     class Meta:
         db_table = 'odata_category'
 
+
 class Customer(models.Model):
     """This model is used for customer"""
     _id = models.ObjectIdField(primary_key=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     email = models.CharField(max_length=200, null=True, blank=True)
-    first_name = models.CharField(max_length=100,null=True,blank=True)
-    last_name = models.CharField(max_length=100,null=True,blank=True)
-    address1 = models.CharField(max_length=100,null=True,blank=True)
-    address2 = models.CharField(max_length=100,null=True,blank=True)
-    city = models.CharField(max_length=100,null=True,blank=True)
-    state = models.CharField(max_length=100,null=True,blank=True)
-    postal_code = models.IntegerField(null=True,blank=True)
-    country = models.CharField(max_length=100,null=True,blank=True)
-    phone = models.CharField(max_length=10,null=True,blank=True)
-    password = models.TextField(max_length=100,null=True)
-    salutation = models.CharField(max_length=100,null=True,blank=True)
-    credit_card = models.CharField(max_length=15, null=True,blank=True)
-    credit_card_type_id = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100, null=True, blank=True)
+    last_name = models.CharField(max_length=100, null=True, blank=True)
+    address1 = models.CharField(max_length=100, null=True, blank=True)
+    address2 = models.CharField(max_length=100, null=True, blank=True)
+    city = models.CharField(max_length=100, null=True, blank=True)
+    state = models.CharField(max_length=100, null=True, blank=True)
+    postal_code = models.IntegerField(null=True, blank=True)
+    country = models.CharField(max_length=100, null=True, blank=True)
+    phone = models.CharField(max_length=10, null=True, blank=True)
+    password = models.TextField(max_length=100, null=True)
+    salutation = models.CharField(max_length=100, null=True, blank=True)
+    credit_card = models.CharField(max_length=15, null=True, blank=True)
+    credit_card_type_id = models.CharField(max_length=100, null=True)
     mm_yy = models.CharField(max_length=7, null=True, blank=True)
-    billing_address = models.CharField(max_length=250, null=True,blank=True)
-    billing_city = models.CharField(max_length=100, null=True,blank=True)
-    billing_postal_code = models.CharField(max_length=100, null=True,blank=True)
-    billing_country = models.CharField(max_length=100, null=True,blank=True)
-    ship_address = models.CharField(max_length=250, null=True,blank=True)
-    ship_city = models.CharField(max_length=250, null=True,blank=True)
-    ship_region = models.CharField(max_length=250, null=True,blank=True)
-    ship_postal_code = models.CharField(max_length=100, null=True,blank=True)
-    ship_country = models.CharField(max_length=100, null=True,blank=True)
+    billing_address = models.CharField(max_length=250, null=True, blank=True)
+    billing_city = models.CharField(max_length=100, null=True, blank=True)
+    billing_postal_code = models.CharField(max_length=100, null=True, blank=True)
+    billing_country = models.CharField(max_length=100, null=True, blank=True)
+    ship_address = models.CharField(max_length=250, null=True, blank=True)
+    ship_city = models.CharField(max_length=250, null=True, blank=True)
+    ship_region = models.CharField(max_length=250, null=True, blank=True)
+    ship_postal_code = models.CharField(max_length=100, null=True, blank=True)
+    ship_country = models.CharField(max_length=100, null=True, blank=True)
     marketing_code = models.CharField(max_length=100, null=True, blank=True)
     source = models.CharField(max_length=100, null=True, blank=True)
     voucher = models.CharField(max_length=10, null=True, blank=True)
+    voucher_value = models.BooleanField(default=False, null=True)
     discount = models.CharField(max_length=10, null=True, blank=True)
+    discount_value = models.BooleanField(default=False, null=True)
     wishlist = models.TextField(null=True)
     checkout = models.TextField(null=True)
     medium = models.CharField(max_length=100, null=True, blank=True)
@@ -105,7 +111,7 @@ class Customer(models.Model):
 
     def __str__(self):
         if self.first_name is not None and self.last_name is not None:
-            return self.first_name+' '+self.last_name
+            return self.first_name + ' ' + self.last_name
         elif self.first_name is not None:
             return self.first_name
         else:
@@ -128,6 +134,7 @@ class UserForgotPassword(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+
 # @receiver(post_save, sender=User)
 # def create_user_customer(sender, instance, created, **kwargs):
 #     if created:
@@ -138,28 +145,28 @@ class UserForgotPassword(models.Model):
 #     instance.customer.save()
 
 
-
 class Product(models.Model):
     """This models is used for Products Details."""
     _id = models.ObjectIdField(primary_key=True)
-    vendor_product_id = models.CharField(max_length=50,null=True,blank=True)
+    vendor_product_id = models.CharField(max_length=50, null=True, blank=True)
     product_name = models.CharField(max_length=100)
     category = models.CharField(max_length=250, default='')
     quantity = models.IntegerField(default=0)
     price = models.FloatField()
-    msrp = models.CharField(max_length=100, null=True,blank=True)
+    msrp = models.CharField(max_length=100, null=True, blank=True)
     ean = models.CharField(max_length=255, null=True, blank=True)
     title = models.CharField(max_length=1024)
     title_de = models.CharField(max_length=1024, blank=True, null=True)
-    # size = models.CharField(max_length=100,null=True,blank=True)
-    # color = models.CharField(max_length=100,null=True,blank=True)
-    discount = models.DecimalField(decimal_places=2, max_digits=10, verbose_name="Discount %",default=0)
+    size = models.CharField(max_length=100, null=True, blank=True)
+    color = models.CharField(max_length=100, null=True, blank=True)
+    color_de = models.CharField(max_length=100, null=True, blank=True)
+    discount = models.DecimalField(decimal_places=2, max_digits=10, verbose_name="Discount %", default=0)
     product_available = models.BooleanField(default=False)
-    picture = models.URLField(null=True,blank=True)
-    ranking = models.CharField(max_length=15,choices=RANKING, null=True,blank=True)
-    description = models.TextField(max_length=200, null=True,blank=True)
+    picture = models.URLField(null=True, blank=True)
+    ranking = models.CharField(max_length=15, choices=RANKING, null=True, blank=True)
+    description = models.TextField(max_length=200, null=True, blank=True)
     description_de = models.TextField(max_length=200, blank=True, null=True)
-    product_highlight = models.TextField(max_length=200, null=True,blank=True)
+    product_highlight = models.TextField(max_length=200, null=True, blank=True)
     product_highlight_de = models.TextField(max_length=200, blank=True, null=True)
     created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True, auto_now_add=False)
@@ -184,7 +191,7 @@ class ProductImage(models.Model):
     _id = models.ObjectIdField(primary_key=True)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     image_order = models.IntegerField()
-    image = models.URLField(null=True,blank=True)
+    image = models.URLField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True, auto_now_add=False)
 
@@ -193,12 +200,21 @@ class ProductVariant(models.Model):
     _id = models.ObjectIdField(primary_key=True)
     parent_product = models.ForeignKey(Product, on_delete=models.CASCADE)
     size = models.CharField(max_length=100)
-    size_variant = models.CharField(max_length=100)
+    size_variant = models.CharField(max_length=100, null=True)
     color = models.CharField(max_length=100)
-    color_variant=models.CharField(max_length=100)
+    color_variant = models.CharField(max_length=100, null=True)
     material = models.CharField(max_length=100)
     ean = models.CharField(max_length=100, null=True)
     image = models.URLField(null=True, blank=True)
+
+    # def __str__(self):
+    #     # product = Product.objects.get(_id=self.parent_product_id)
+    #     # print(product)
+    #     # if product.product_name is None:
+    #     #     return ""
+    #     # return self.product.product_name
+    #     return "Try this"
+
 
 # class Shipper(models.Model):
 #     _id = models.ObjectIdField(primary_key=True)
@@ -248,7 +264,6 @@ class Payment(models.Model):
         if customer.first_name is None or customer.last_name is None:
             return ""
         return customer.first_name + ' ' + customer.last_name
-
 
 
 # class OrderDetail(models.Model):
